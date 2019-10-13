72e92ebdf4465d332f69329bd66978f63a2880a9
Nimrod Shneor <nshneor@redhat.com>
2019-10-13 11:33:08 +0300

    Adding HeapSort and testing for it

--- heap/max_heap.go
+++ heap/max_heap.go
@@ -28,6 +28,10 @@ func (h *MaxHeap) Size() int {
 	return len(h.heap) - 1
 }
 
+func (h *MaxHeap) Array() []int {
+	return h.heap
+}
+
 func MaxHeapify(h MaxHeap, i int) {
 	largest := 0
 	l := h.Left(i)
@@ -37,7 +41,7 @@ func MaxHeapify(h MaxHeap, i int) {
 	} else {
 		largest = i
 	}
-	if r <= h.Size() && h.Get(r) > largest {
+	if r <= h.Size() && h.Get(r) > h.Get(largest) {
 		largest = r
 	}
 	if largest != i {
@@ -53,8 +57,22 @@ func BuildMaxHeap(input []int) *MaxHeap {
 	h := MaxHeap{
 		heap: input,
 	}
-	for i := (len(input) / 2); i >= 0; i-- {
+	for i := (len(input) - 1) / 2; i >= 0; i-- {
 		MaxHeapify(h, i)
 	}
 	return &h
+}
+
+// Sort performs a heap-sort of array input.
+func Sort(input []int) []int {
+	heap := BuildMaxHeap(input)
+	result := make([]int, len(input))
+	for i := heap.Size(); i > 0; i-- {
+		result[i] = heap.Get(0)
+		heap.Set(0, heap.Get(i))
+		heap.heap = heap.heap[:i]
+		MaxHeapify(*heap, 0)
+	}
+	result[0] = heap.Get(0)
+	return result
 }
--- heap/max_heap_test.go
+++ heap/max_heap_test.go
@@ -26,8 +26,49 @@ func TestBuildMaxHeap(t *testing.T) {
 			t.Fatalf("Test Failed: expected %v, instead recieved: %v",
 				test.expected, result)
 		}
-		for i := range result.heap {
+		for i := range result.Array() {
 			if result.Get(i) != test.expected.Get(i) {
+				t.Fatalf("Test Failed: expected %v, instead recieved: %v",
+					test.expected, result)
+			}
+		}
+	}
+}
+
+func TestHeapSort(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected []int
+	}{
+		{
+			input:    []int{1, 2, 3},
+			expected: []int{1, 2, 3},
+		},
+		{
+			input:    []int{3, 2, 1},
+			expected: []int{1, 2, 3},
+		},
+		{
+			input:    []int{1},
+			expected: []int{1},
+		},
+		{
+			input:    []int{1, 2, 3, 7, 6, 9, 4},
+			expected: []int{1, 2, 3, 4, 6, 7, 9},
+		},
+		{
+			input:    []int{31, 41, 59, 26, 41, 58},
+			expected: []int{26, 31, 41, 41, 58, 59},
+		},
+	}
+	for _, test := range tests {
+		result := Sort(test.input)
+		if len(result) != len(test.expected) {
+			t.Fatalf("Test Failed: expected %v, instead recieved: %v",
+				test.expected, result)
+		}
+		for i := range result {
+			if result[i] != test.expected[i] {
 				t.Fatalf("Test Failed: expected %v, instead recieved: %v",
 					test.expected, result)
 			}
