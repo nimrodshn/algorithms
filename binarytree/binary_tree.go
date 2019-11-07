666806b86eaff5e804001b639041127a6ce70c6b
Nimrod Shneor <nshneor@redhat.com>
2019-11-07 11:48:50 +0200

    Adding Delete method to Binary Tree

--- binarytree/binary_tree.go
+++ binarytree/binary_tree.go
@@ -7,9 +7,10 @@ type BinaryTree struct {
 
 // Node represents a node in the binary tree.
 type Node struct {
-	key   int
-	left  *Node
-	right *Node
+	key         int
+	predecessor *Node
+	left        *Node
+	right       *Node
 }
 
 // Min returns the node with the minimum key.
@@ -62,6 +63,7 @@ func (b *BinaryTree) Insert(node *Node) {
 			x = x.right
 		}
 	}
+	node.predecessor = pred
 	if pred == nil {
 		b.root = pred
 	}
@@ -69,5 +71,65 @@ func (b *BinaryTree) Insert(node *Node) {
 		pred.left = node
 	} else {
 		pred.right = node
+	}
+}
+
+// Successor returns the successor of a given node (with respect to its key).
+func (b *BinaryTree) Successor(node *Node) *Node {
+	if node.right != nil {
+		rightSubTree := BinaryTree{node.right}
+		return rightSubTree.Min()
+	}
+	y := node.predecessor
+	for y != nil && node == y.right {
+		node = y
+		y = y.predecessor
+	}
+	return y
+}
+
+// Delete delets a node from the binary tree.
+func (b *BinaryTree) Delete(node *Node) {
+	y := node.predecessor
+	// Both of the nodes children are empty (i.e. node is a leaf).
+	// We simply remove the node.
+	if node.right == nil && node.left == nil {
+		if y.right == node {
+			y.right = nil
+		} else {
+			y.left = nil
+		}
+		return
+	}
+	// One of the nodes children is nil.
+	// Splice out the node and attach its non-nil child to his parent.
+	if node.right == nil {
+		if y.right == node {
+			y.right = node.left
+		} else {
+			y.left = node.left
+		}
+		return
+	} else if node.left == nil {
+		if y.right == node {
+			y.right = node.right
+		} else {
+			y.left = node.right
+		}
+		return
+	}
+	// Both of the nodes are non-nil.
+	successor := b.Successor(node)
+	var x *Node
+	// fix childrens predecessor.
+	if y.left != nil {
+		x = successor.left
+	} else {
+		x = successor.right
+	}
+	x.predecessor = successor.predecessor
+	if successor != node {
+		// replace the node with its successor.
+		node.key = successor.key
 	}
 }
